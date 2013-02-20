052940ecbac95ff40357fdd9e5eff7e9b0e50647
Luke Orland <orluke@gmail.com>
2013-02-20 10:35:05 -0500

    add submitted view, update HIT with result string, mark completed.

--- hits/views.py
+++ hits/views.py
@@ -4,15 +4,21 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import Context, loader, RequestContext
 from hits.models import Hit
 
-def index(request):
-    unfinished_hit_list = Hit.objects.filter(completed=False).order_by('-id')
+def hits_list_context(template, more_map={}):
+    unfinished_hit_list = Hit.objects.filter(completed=False).order_by('id')
     finished_hit_list = Hit.objects.filter(completed=True).order_by('-id')
+    c = Context(
+            dict({'unfinished_hit_list': unfinished_hit_list,
+                  'finished_hit_list': finished_hit_list},
+                 **more_map
+            )
+    )
+
+    return template.render(c)
+
+def index(request):
     t = loader.get_template('hits/index.html')
-    c = Context({
-        'unfinished_hit_list': unfinished_hit_list,
-        'finished_hit_list': finished_hit_list,
-    })
-    return HttpResponse(t.render(c))
+    return HttpResponse(hits_list_context(t))
 
 def detail(request, hit_id):
     h = get_object_or_404(Hit, pk=hit_id)
@@ -23,5 +29,10 @@ def results(request, hit_id):
     return HttpResponse("You're looking at the results of hit %s." % hit_id)
 
 def submission(request, hit_id):
-    print(str(request.body))
-    return HttpResponse("hola")
+    h = get_object_or_404(Hit, pk=hit_id)
+    h.completed = True
+    h.answers = str(request.body)
+    h.save()
+    # Show link to all HITs (hits/index.html)
+    t = loader.get_template('hits/submission.html')
+    return HttpResponse(hits_list_context(t, {'submitted_hit': h}))
