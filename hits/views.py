0aa365a96796a35249528c4d1a9b7011742c9f15
Luke Orland <orluke@gmail.com>
2013-02-20 11:20:18 -0500

    clean out unused imports

--- hits/views.py
+++ hits/views.py
@@ -1,5 +1,4 @@
-from django.core.urlresolvers import reverse
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import Context, loader, RequestContext
 from hits.models import Hit
