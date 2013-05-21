bacbbfe4a267624e040e62437f72ca1d34e6acb5
Luke Orland <orluke@gmail.com>
2013-05-20 22:24:40 -0400

    pep8 improvements

--- hits/tests.py
+++ hits/tests.py
@@ -87,9 +87,8 @@ class TestGenerateForm(TestCase):
 
     def setUp(self):
         self.hit_template = HitTemplate(
-                source_file="filepath",
-                form="""
-<p>
+            source_file="filepath",
+            form="""<p>
 <meta charset="utf-8">
 <table cellspacing="0" cellpadding="6" border="1">
     <tbody>
@@ -133,8 +132,8 @@ class TestGenerateForm(TestCase):
         )
         self.hit_template.save()
         self.hit = Hit(
-            source_file = "filepath",
-            source_line = 1,
+            source_file="filepath",
+            source_line=1,
             form=self.hit_template,
             input_csv_fields="tweet0_id,tweet0_entity,tweet0_before_entity,tweet0_after_entity,tweet0_word0,tweet0_word1,tweet0_word2,tweet1_id,tweet1_entity,tweet1_before_entity,tweet1_after_entity,tweet1_word0,tweet1_word1,tweet1_word2,tweet2_id,tweet2_entity,tweet2_before_entity,tweet2_after_entity,tweet2_word0,tweet2_word1,tweet2_word2,tweet3_id,tweet3_entity,tweet3_before_entity,tweet3_after_entity,tweet3_word0,tweet3_word1,tweet3_word2,tweet4_id,tweet4_entity,tweet4_before_entity,tweet4_after_entity,tweet4_word0,tweet4_word1,tweet4_word2,tweet5_id,tweet5_entity,tweet5_before_entity,tweet5_after_entity,tweet5_word0,tweet5_word1,tweet5_word2",
             input_csv_values="268,SANTOS, Muy bien America ......... y lo siento mucho , un muy buen rival,mucho,&nbsp;,&nbsp;,2472,GREGORY, Ah bueno , tampoco andes pidiendo ese tipo de milagros . @jcabrerac @CarlosCabreraR,bueno,&nbsp;,&nbsp;,478,ALEJANDRO, @aguillen19 &#44; un super abrazo mi querido , &#44; mis mejores deseos para este 2012 ... muakkk !,querido,&nbsp;,&nbsp;,906_control, PF, Acusan camioneros extorsiones de, : Transportistas acusaron que deben pagar entre 13 y 15 mil pesos a agentes que .. http://t.co/d8LUVvhP,acusaron,&nbsp;,&nbsp;,2793_control, CHICARO, Me gusta cuando chicharo hace su oracion es lo que lo hace especial .,&nbsp;,gusta,&nbsp;,&nbsp;,357,OSCAR WILDE&QUOT;, &quot; @ ifilosofia : Las pequeñas acciones de cada día son las que hacen o deshacen el carácter.&quot; , bueno !!!! Es así,bueno,&nbsp;,&nbsp;",
@@ -142,8 +141,7 @@ class TestGenerateForm(TestCase):
         self.hit.save()
 
     def test_generate_form(self):
-        expect = """
-<p>
+        expect = """<p>
 <meta charset="utf-8">
 <table cellspacing="0" cellpadding="6" border="1">
     <tbody>
@@ -194,8 +192,8 @@ class TestGenerateForm(TestCase):
         )
         hit_template.save()
         hit = Hit(
-            source_file = "filepath",
-            source_line = 1,
+            source_file="filepath",
+            source_line=1,
             form=hit_template,
             input_csv_fields="tweet0_id,tweet0_entity",
             input_csv_values="268,SANTOS"
