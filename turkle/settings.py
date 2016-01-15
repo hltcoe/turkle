94a0e8e7d269d7c1c4d4301bbfe013be91a97b57
Chandler May <cjmay4754@gmail.com>
2016-01-15 15:44:59 -0500

    comma

--- turkle/settings.py
+++ turkle/settings.py
@@ -110,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.getcwd(), 'templates')
+    os.path.join(os.getcwd(), 'templates'),
 )
 
 INSTALLED_APPS = (
