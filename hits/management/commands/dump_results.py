e04a39a113b5cbe7251804e95b4b0b49a60ea59c
Chandler May <cjmay4754@gmail.com>
2016-02-25 15:38:02 -0500

    Extend dump_results to write a different result file per template/field-name-list.

--- hits/management/commands/dump_results.py
+++ hits/management/commands/dump_results.py
@@ -8,15 +8,35 @@ from hits.models import HitTemplate
 from unicodecsv import DictWriter
 
 
+def get_fieldnames(hit):
+    return tuple(sorted(
+        [u'Input.' + k for k in hit.input_csv_fields.keys()]
+        + [u'Answer.' + k for k in hit.answers.keys()]
+    ))
+
+
+def results_data_groups(completed_hits):
+    hit_fieldname_tuples = map(get_fieldnames, completed_hits)
+    fieldname_tuple_id_map = dict(
+        (t, i)
+        for (i, t)
+        in enumerate(sorted(set(hit_fieldname_tuples)))
+    )
+
+    hit_groups = [[] for t in fieldname_tuple_id_map] 
+    for (hit, fieldnames) in zip(completed_hits, hit_fieldname_tuples):
+        i = fieldname_tuple_id_map[fieldnames]
+        hit_groups[i].append(hit)
+
+    return map(results_data, hit_groups)
+
+
 def results_data(completed_hits):
     """
     All completed HITs must come from the same template so that they have the
     same field names.
     """
-    fieldnames = sorted(
-        [u'Input.' + k for k in completed_hits[0].input_csv_fields.keys()]
-        + [u'Answer.' + k for k in completed_hits[0].answers.keys()]
-    )
+    fieldnames = get_fieldnames(completed_hits[0])
 
     rows = []
     for hit in completed_hits:
@@ -32,9 +52,12 @@ class Command(BaseCommand):
 
     args = '<template_file_path> <results_csv_file_path>'
     help = (
-        'Dumps results of the completed HITs for the template '
-        '<template_file_path> '
-        'to a file at <results_csv_file_path>'
+        'Dumps results of the completed HITs for the template'
+        ' <template_file_path> to a file at <results_csv_file_path>. '
+		' If <template_file_path> is * then results will be written to'
+		' a separate file for each template and unique set of'
+		' fieldnames (<results_csv_file_path> will be interpreted as a'
+		' prefix for the individual results files).'
     )
 
     def handle(self, *args, **options):
@@ -45,21 +68,43 @@ class Command(BaseCommand):
                 '<results_csv_file_path>'
             )
 
-        # Get paths from args, and normalize them to absolute paths:
-        template_file_path, results_csv_file_path = map(os.path.abspath, args)
+        (template_file_path, results_csv_file_path) = args
 
-        try:
-            template = HitTemplate.objects.get(name=template_file_path)
-        except ObjectDoesNotExist:
-            sys.exit('There is no matching <template_file_path>.')
+        if template_file_path == '*':
+            results_idx = 0
+            for template in HitTemplate.objects.all():
+                completed_hits = template.hit_set.filter(completed=True)
 
-        completed_hits = template.hit_set.filter(completed=True)
-        if not completed_hits.exists():
-            sys.exit('There are no completed HITs.')
+                if completed_hits.exists():
+                    groups = results_data_groups(completed_hits)
+                    for (fieldnames, rows) in groups:
+                        real_results_path = (
+                            '%s.%d.csv' % (results_csv_file_path, results_idx)
+                        )
+                        with open(real_results_path, 'wb') as fh:
+                            writer = DictWriter(fh, fieldnames)
+                            writer.writeheader()
+                            for row in rows:
+                                writer.writerow(row)
 
-        fieldnames, rows = results_data(completed_hits)
-        with open(results_csv_file_path, 'wb') as fh:
-            writer = DictWriter(fh, fieldnames)
-            writer.writeheader()
-            for row in rows:
-                writer.writerow(row)
+                        results_idx += 1
+
+        else:
+            template_file_path = os.path.abspath(template_file_path)
+            results_csv_file_path = os.path.abspath(results_csv_file_path)
+
+            try:
+                template = HitTemplate.objects.get(name=template_file_path)
+            except ObjectDoesNotExist:
+                sys.exit('There is no matching <template_file_path>.')
+
+            completed_hits = template.hit_set.filter(completed=True)
+            if not completed_hits.exists():
+                sys.exit('There are no completed HITs.')
+
+            (fieldnames, rows) = results_data(completed_hits)
+            with open(results_csv_file_path, 'wb') as fh:
+                writer = DictWriter(fh, fieldnames)
+                writer.writeheader()
+                for row in rows:
+                    writer.writerow(row)
