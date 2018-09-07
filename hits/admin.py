e1c5b161abf61913b8f104373e04c71f5df1fe8a
Craig Harman <craig@craigharman.net>
2018-09-07 14:04:46 -0400

    Added customized admin interface for HitBatch, HitTemplate data

--- hits/admin.py
+++ hits/admin.py
@@ -1,6 +1,21 @@
-from hits.models import Hit, HitBatch, HitTemplate
 from django.contrib import admin
+from django.db import models
+from django.forms import TextInput
+
+from hits.models import Hit, HitBatch, HitTemplate
+
+
+class HitBatchAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'60'})},
+    }
+
+class HitTemplateAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'60'})},
+    }
+
 
 admin.site.register(Hit)
-admin.site.register(HitBatch)
-admin.site.register(HitTemplate)
+admin.site.register(HitBatch, HitBatchAdmin)
+admin.site.register(HitTemplate, HitTemplateAdmin)
