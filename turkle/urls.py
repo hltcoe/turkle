7c570a7f1ee644e0d63ccaa5c06a94b77c008749
Craig Harman <craig@craigharman.net>
2018-09-18 13:03:24 -0400

    Added django.contrib.auth views

--- turkle/urls.py
+++ turkle/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+from django.contrib import admin, auth
 
 import turkle.views
 
@@ -16,4 +16,6 @@ urlpatterns = [
     url(r'^hits/', include('hits.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url('^', include('django.contrib.auth.urls')),
 ]
