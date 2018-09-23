b52c81bf7aebae70b458ecd8bc03c6dd0abf4437
Craig Harman <craig@craigharman.net>
2018-09-23 16:38:17 -0400

    Added 'accept_hit', 'preview', 'preview_iframe' views

--- hits/urls.py
+++ hits/urls.py
@@ -1,19 +1,25 @@
 from django.conf.urls import url
 
 from hits.views import (
+    accept_hit,
     accept_next_hit,
     download_batch_csv,
     hit_assignment,
     hit_assignment_iframe,
     index,
+    preview,
+    preview_iframe,
 )
 
 urlpatterns = [
     url(r'^$', index, name='index'),
+    url(r'^(?P<hit_id>\d+)/$', preview, name='preview'),
+    url(r'^(?P<hit_id>\d+)/iframe/$', preview_iframe, name='preview_iframe'),
     url(r'^(?P<hit_id>\d+)/assignment/(?P<hit_assignment_id>\d+)/$',
         hit_assignment, name='hit_assignment'),
     url(r'^(?P<hit_id>\d+)/assignment/iframe/(?P<hit_assignment_id>\d+)/$',
         hit_assignment_iframe, name='hit_assignment_iframe'),
+    url(r'^batch/(?P<batch_id>\d+)/accept_hit/(?P<hit_id>\d+)/$', accept_hit, name='accept_hit'),
     url(r'^batch/(?P<batch_id>\d+)/accept_next_hit/$', accept_next_hit, name='accept_next_hit'),
     url(r'^batch/(?P<batch_id>\d+)/download/$', download_batch_csv, name='download_batch_csv'),
 ]
