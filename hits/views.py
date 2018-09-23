a708e84290fb3044e75cc3d1d4448def95244213
Craig Harman <craig@craigharman.net>
2018-09-22 20:09:33 -0400

    HIT table in index view now displays correctly if no HITs for user

--- hits/views.py
+++ hits/views.py
@@ -87,12 +87,14 @@ def _get_batch_table_rows(request):
     batch_rows = []
     for hit_template in HitTemplate.available_for(request.user):
         for hit_batch in hit_template.batches_available_for(request.user):
-            batch_rows.append({
-                'template_name': hit_template.name,
-                'batch_name': hit_batch.name,
-                'batch_published': hit_batch.date_published,
-                'assignments_available': hit_batch.total_available_hits_for(request.user),
-                'accept_next_hit_url': reverse('accept_next_hit',
-                                               kwargs={'batch_id': hit_batch.id})
-            })
+            total_hits_available = hit_batch.total_available_hits_for(request.user)
+            if total_hits_available > 0:
+                batch_rows.append({
+                    'template_name': hit_template.name,
+                    'batch_name': hit_batch.name,
+                    'batch_published': hit_batch.date_published,
+                    'assignments_available': total_hits_available,
+                    'accept_next_hit_url': reverse('accept_next_hit',
+                                                   kwargs={'batch_id': hit_batch.id})
+                })
     return batch_rows
