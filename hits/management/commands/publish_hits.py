b2917554ba3a2836f6398a9fb686f4e1dfeb7e42
Craig Harman <craig@craigharman.net>
2018-09-23 11:34:18 -0400

    Added `--template-name` and `--batch-name` args to `publish_hits.py`

--- hits/management/commands/publish_hits.py
+++ hits/management/commands/publish_hits.py
@@ -6,10 +6,13 @@ from django.core.management.base import BaseCommand
 from hits.models import HitBatch, HitTemplate
 
 
-def get_or_create_template_from_html_file(htmlfile, template_file_path):
+def get_or_create_template_from_html_file(htmlfile, template_file_path, template_name):
     template_file_path = os.path.abspath(template_file_path)
     filename = template_file_path
-    name = template_file_path
+    if template_name:
+        name = template_name
+    else:
+        name = template_file_path
     form = htmlfile.read().decode('utf-8')
 
     template, created = HitTemplate.objects.get_or_create(
@@ -33,6 +36,8 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('template_file_path', type=str)
         parser.add_argument('csv_file_path', type=str)
+        parser.add_argument('--batch_name', type=str, default='')
+        parser.add_argument('--template_name', type=str, default='')
 
     def handle(self, *args, **options):
         template_file_path = os.path.abspath(options['template_file_path'])
@@ -41,15 +46,20 @@ class Command(BaseCommand):
         with open(template_file_path, 'rb') as fh:
             hit_template = get_or_create_template_from_html_file(
                 fh,
-                template_file_path
+                template_file_path,
+                options['template_name'],
             )
 
         with open(csv_file_path, 'rb') as fh:
             sys.stderr.write('Creating HITs: ')
+            if options['batch_name']:
+                batch_name = options['batch_name']
+            else:
+                batch_name = csv_file_path
             hit_batch = HitBatch(
                 hit_template=hit_template,
                 filename=csv_file_path,
-                name=csv_file_path
+                name=batch_name
             )
             hit_batch.save()
 
