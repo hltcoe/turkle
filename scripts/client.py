ba83f6dd200425e14ddfbb7814523e443f867112
Cash Costello <cash.costello@jhuapl.edu>
2018-09-27 14:27:11 -0400

    adds publish hits script

--- scripts/client.py
+++ scripts/client.py
@@ -1,5 +1,7 @@
 import functools
 import getpass
+import os
+import re
 import requests
 
 
@@ -17,6 +19,8 @@ def exception_handler(function):
 class TurkleClient(object):
     LOGIN_URL = "/login/"
     ADD_USER_URL = "/admin/auth/user/add/"
+    ADD_TEMPLATE_URL = "/admin/hits/hittemplate/add/"
+    ADD_BATCH_URL = "/admin/hits/hitbatch/add/"
 
     def __init__(self, args):
         if not args.p:
@@ -41,6 +45,96 @@ class TurkleClient(object):
                 print("Error: username already exists")
                 return False
         return True
+
+    @exception_handler
+    def publish(self):
+        if not self.validate_publish():
+            return False
+
+        self.prepare_publish()
+
+        with requests.Session() as session:
+            if not self.login(session):
+                return False
+            if not self.upload_template(session):
+                return False
+            return self.upload_csv(session)
+
+    def upload_template(self, session):
+        url = self.format_url(self.ADD_TEMPLATE_URL)
+        session.get(url)
+        payload = {
+            'name': self.args.template_name,
+            'assignments_per_hit': self.args.num,
+            'filename': self.args.template,
+            'form': self.args.form,
+            'active': 1,
+            'csrfmiddlewaretoken': session.cookies['csrftoken']
+        }
+        if self.args.login:
+            payload['login_required'] = 1
+        resp = session.post(url, data=payload)
+        if resp.status_code != requests.codes.ok:
+            print("Error: uploading the template failed")
+            return False
+        return True
+
+    def upload_csv(self, session):
+        url = self.format_url(self.ADD_BATCH_URL)
+        resp = session.get(url)
+        # grab a list of the template ids from the form
+        regex = r'<option value="(.*)">'
+        ids = re.findall(regex, resp.text)
+        payload = {
+            # we just upload a template so we assume that its last in list
+            'hit_template': ids[-1],
+            'name': self.args.batch_name,
+            'assignments_per_hit': self.args.num,
+            'active': 1,
+            'csrfmiddlewaretoken': session.cookies['csrftoken']
+        }
+        files = {'csv_file': (self.args.csv, self.args.csv_data)}
+        resp = session.post(url, data=payload, files=files)
+        if resp.status_code != requests.codes.ok:
+            print("Error: uploading the csv failed")
+            return False
+        return True
+
+    def validate_publish(self):
+        # HITs that don't require a log in can only be done once
+        if not self.args.login and self.args.num != 1:
+            print("Error: login cannot be off if more than 1 assignment per hit")
+            return False
+
+        if not os.path.exists(self.args.template):
+            print("Error: template path does not exist")
+            return False
+
+        if not os.path.exists(self.args.csv):
+            print("Error: csv path does not exist")
+            return False
+
+        return True
+
+    def prepare_publish(self):
+        if not self.args.template_name:
+            self.args.template_name = self.extract_name(self.args.template)
+        if not self.args.batch_name:
+            self.args.batch_name = self.extract_name(self.args.csv)
+        self.args.form = self.read_file(self.args.template)
+        self.args.template = os.path.basename(self.args.template)
+        self.args.csv_data = self.read_file(self.args.csv)
+        self.args.csv = os.path.basename(self.args.csv)
+
+    @staticmethod
+    def read_file(filename):
+        with open(filename, "r") as fh:
+            data = fh.read()
+            return data
+
+    @staticmethod
+    def extract_name(filename):
+        return os.path.splitext(os.path.basename(filename))[0]
 
     def login(self, session):
         result = True
---
+++ scripts/publish_hits.py
@@ -0,0 +1,26 @@
+import argparse
+from client import TurkleClient
+import sys
+
+
+parser = argparse.ArgumentParser(
+    description="Publish a batch of HITs to Turkle",
+    epilog="Requires a template and the batch CSV"
+)
+parser.add_argument("-u", help="admin username", required=True)
+parser.add_argument("-p", help="admin password")
+parser.add_argument("--server", help="hostname:port", default="localhost:8000")
+parser.add_argument("--num", help="number of assignments per hit", default=1, type=int)
+parser.add_argument("--login", help="is login required (0 or 1)", default=1, type=int, choices=[0, 1])
+parser.add_argument("--template-name", help="Name of the task")
+parser.add_argument("--batch-name", help="Name of the batch")
+parser.add_argument("template", help="html template file")
+parser.add_argument("csv", help="csv file for batch")
+args = parser.parse_args()
+
+client = TurkleClient(args)
+result = client.publish()
+if result:
+    print("Success")
+else:
+    sys.exit(1)
