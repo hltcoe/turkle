ad8b10398433deee31d88e268c65897bef85d6f0
Cash Costello <cash.costello@jhuapl.edu>
2018-10-03 16:27:25 -0400

    using MIDDLEWARE rather than MIDDLEWARE_CLASSES. Django 1.10 deprecation.

--- turkle/settings.py
+++ turkle/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
