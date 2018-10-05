d6a925dac42a7146c1dee74c8e137a4e8846d1ed
Cash Costello <cash.costello@jhuapl.edu>
2018-10-05 10:16:42 -0400

    added integration test for downloading finished tasks

--- hits/tests/test_client.py
+++ hits/tests/test_client.py
@@ -1,14 +1,23 @@
 import django.test
+import os
+import tempfile
 from scripts.client import TurkleClient
 
 
 class TestClient(django.test.LiveServerTestCase):
-    fixtures = ['hits/tests/resources/user.json']
+    fixtures = ['hits/tests/resources/test_db.json']
+
+    def setUp(self):
+        self.client = TurkleClient(self.live_server_url, "admin", "password")
 
     def test_login_failure(self):
         client = TurkleClient(self.live_server_url, "admin", "chicken")
         self.assertFalse(client.add_user("tony", "password"))
 
     def test_add_user(self):
-        client = TurkleClient(self.live_server_url, "admin", "password")
-        self.assertTrue(client.add_user("tony", "password"))
+        self.assertTrue(self.client.add_user("tony", "password"))
+
+    def test_download(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            self.client.download(tmpdir)
+            self.assertTrue(os.path.exists(os.path.join(tmpdir, "sent-Batch_1_results.csv")))
