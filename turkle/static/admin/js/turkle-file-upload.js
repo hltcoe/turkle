52efcb604542105342973c7b55cf50d8e4fb6d4b
Cash Costello <cash.costello@jhuapl.edu>
2019-11-19 13:29:13 -0500

    moves most inline javascript to static files

---
+++ turkle/static/admin/js/turkle-file-upload.js
@@ -0,0 +1,39 @@
+
+/* Used on edit forms for batches and projects */
+$(function () {
+  document.addEventListener('dragenter', (e) => {
+    e.preventDefault();
+  });
+
+  document.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+
+  document.addEventListener('drop', (e) => {
+    e.preventDefault();
+    copyFileTextToFormField(e.dataTransfer.files[0]);
+  });
+
+  $('#id_template_file_upload').change(function (e) {
+    copyFileTextToFormField(this.files[0]);
+
+    // Resetting input value allows the user to upload same file twice
+    $(this).val('');
+  });
+
+  function copyFileTextToFormField(f) {
+    var reader = new FileReader();
+    reader.onload = (function(theFile) {
+      var fileContents = theFile.target.result;
+      $('#id_html_template').val(fileContents);
+      $('#project_form').parsley().validate({group: 'html_template'});
+    });
+    reader.readAsText(f);
+
+    // DOM ID created in custom_button_file_widget.html
+    $('#id_template_file_upload_custom_text').text(f.name);
+
+    // Update hidden form field
+    $('#id_filename').val(f.name);
+  }
+});
