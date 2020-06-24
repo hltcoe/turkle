f49a43aac9800f860324da9902156eef53a2700f
Cash Costello <cash.costello@jhuapl.edu>
2020-06-24 15:36:00 -0400

    tell read the docs to look at index.rst for contents

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,8 @@ release = turkle.__version__
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
