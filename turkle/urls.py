b50ecc05fec3b1b7f00ab452c721b1798742afaf
Cash Costello <cash.costello@jhuapl.edu>
2021-02-16 10:36:48 -0500

    uses new style URLs

--- turkle/urls.py
+++ turkle/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import (
     accept_task,
@@ -18,25 +18,22 @@ from .views import (
 )
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^stats/$', stats, name='stats'),
-    url(r'^help/$', help_page, name='help'),
-    url(r'^update_auto_accept/$', update_auto_accept, name='update_auto_accept'),
-    url(r'^task/(?P<task_id>\d+)/$', preview, name='preview'),
-    url(r'^task/(?P<task_id>\d+)/iframe/$', preview_iframe, name='preview_iframe'),
-    url(r'^task/(?P<task_id>\d+)/assignment/(?P<task_assignment_id>\d+)/return/$',
+    path('', index, name='index'),
+    path('stats/', stats, name='stats'),
+    path('help/', help_page, name='help'),
+    path('update_auto_accept/', update_auto_accept, name='update_auto_accept'),
+    path('task/<int:task_id>/', preview, name='preview'),
+    path('task/<int:task_id>/iframe/', preview_iframe, name='preview_iframe'),
+    path('task/<int:task_id>/assignment/<int:task_assignment_id>/return/',
         return_task_assignment, name='return_task_assignment'),
-    url(r'^task/(?P<task_id>\d+)/assignment/(?P<task_assignment_id>\d+)/$',
-        task_assignment, name='task_assignment'),
-    url(r'^task/(?P<task_id>\d+)/assignment/iframe/(?P<task_assignment_id>\d+)/$',
+    path('task/<int:task_id>/assignment/<int:task_assignment_id>/',
+         task_assignment, name='task_assignment'),
+    path('task/<int:task_id>/assignment/iframe/<int:task_assignment_id>/',
         task_assignment_iframe, name='task_assignment_iframe'),
-    url(r'^batch/(?P<batch_id>\d+)/accept_task/(?P<task_id>\d+)/$',
-        accept_task, name='accept_task'),
-    url(r'^batch/(?P<batch_id>\d+)/skip_task/(?P<task_id>\d+)/$', skip_task, name='skip_task'),
-    url(r'^batch/(?P<batch_id>\d+)/skip_and_accept_next_task/(?P<task_id>\d+)/' +
-        r'(?P<task_assignment_id>\d+)/$',
+    path('batch/<int:batch_id>/accept_task/<int:task_id>/', accept_task, name='accept_task'),
+    path('batch/<int:batch_id>/skip_task/<int:task_id>/', skip_task, name='skip_task'),
+    path('batch/<int:batch_id>/skip_and_accept_next_task/<int:task_id>/<int:task_assignment_id>/',
         skip_and_accept_next_task, name='skip_and_accept_next_task'),
-    url(r'^batch/(?P<batch_id>\d+)/accept_next_task/$', accept_next_task, name='accept_next_task'),
-    url(r'^batch/(?P<batch_id>\d+)/preview_next_task/$',
-        preview_next_task, name='preview_next_task'),
+    path('batch/<int:batch_id>/accept_next_task/', accept_next_task, name='accept_next_task'),
+    path('batch/<int:batch_id>/preview_next_task/', preview_next_task, name='preview_next_task'),
 ]
--- turkle_site/urls.py
+++ turkle_site/urls.py
@@ -1,40 +1,24 @@
-from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
+from django.urls import include, path
 
 from turkle.admin import admin_site
-import turkle.views
-from turkle_site.settings import TURKLE_EMAIL_ENABLED
 
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', turkle.views.index, name='index'),
+    # backward compatibility - remove with Turkle 3.0 release
+    path('turkle/', include('turkle.urls')),
+
+    path('admin/', admin_site.urls),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('turkle.urls')),
 
     # Route based on this "Failproof Favicons" blog post:
     #   http://staticfiles.productiondjango.com/blog/failproof-favicons/
-    url(
-        r'^favicon.ico$',
-        RedirectView.as_view(
-            url=staticfiles_storage.url('favicon.ico'),
-            permanent=False),
-        name="favicon"
-    ),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    url(r'^turkle/', include('turkle.urls')),
-
-    url(r'^admin/', admin_site.urls),
-
-    url(r'^login/$',
-        auth_views.LoginView.as_view(
-            extra_context={'TURKLE_EMAIL_ENABLED': TURKLE_EMAIL_ENABLED}
-        ),
-        name="login"),
-    url(r'^', include('django.contrib.auth.urls')),
+    path('favicon.ico',
+         RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=False),
+         name="favicon"),
 ]
