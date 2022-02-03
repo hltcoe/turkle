27891f65d839f03003bba51304ce3ecf0d5dee11
Cash Costello <cash.costello@jhuapl.edu>
2022-02-03 08:14:40 -0500

    remove empty favicon

--- turkle_site/urls.py
+++ turkle_site/urls.py
@@ -15,10 +15,4 @@ urlpatterns = [
     path('admin/', admin_site.urls),
     path('', include('django.contrib.auth.urls')),
     path('', include('turkle.urls')),
-
-    # Route based on this "Failproof Favicons" blog post:
-    #   http://staticfiles.productiondjango.com/blog/failproof-favicons/
-    path('favicon.ico',
-         RedirectView.as_view(url=staticfiles_storage.url('favicon.ico'), permanent=False),
-         name="favicon"),
 ]
