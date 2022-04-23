cb2c4153eb782c1940d8701d2913dee5e6410cbe
Cash Costello <cash.costello@gmail.com>
2022-04-23 07:57:34 -0400

    standardize how we get settings in the utilities

--- turkle/utils.py
+++ turkle/utils.py
@@ -4,18 +4,12 @@ from . import __version__
 
 
 def get_site_name():
-    try:
-        return settings.TURKLE_SITE_NAME
-    except AttributeError:
-        pass
-    return 'Turkle'
+    return getattr(settings, 'TURKLE_SITE_NAME', 'Turkle')
 
 
 def get_turkle_template_limit(in_bytes=False):
-    try:
-        template_size_limit = settings.TURKLE_TEMPLATE_LIMIT
-    except AttributeError:
-        template_size_limit = 64
+    """get template size limit in kb by default"""
+    template_size_limit = getattr(settings, 'TURKLE_TEMPLATE_LIMIT', 64)
     if in_bytes:
         template_size_limit *= 1024
     return template_size_limit
@@ -23,14 +17,12 @@ def get_turkle_template_limit(in_bytes=False):
 
 def turkle_vars(request):
     """add variables to the template context"""
-    meta_tags = settings.META_TAGS if hasattr(settings, 'META_TAGS') else []
-
     return {
         'turkle_site_name': get_site_name(),
         'turkle_template_limit': get_turkle_template_limit(),
         'turkle_version': __version__,
-        'turkle_email_enabled': settings.TURKLE_EMAIL_ENABLED,
-        'turkle_meta_tags': meta_tags,
+        'turkle_email_enabled': getattr(settings, 'TURKLE_EMAIL_ENABLED', False),
+        'turkle_meta_tags': getattr(settings, 'META_TAGS', []),
     }
 
 
