ec8eafcce7c6cca6a2f492e68fe44888af2862c8
Cash Costello <cash.costello@jhuapl.edu>
2022-11-08 13:16:05 -0500

    sets the default primary key type as recommended by django 3.2

--- turkle_site/settings.py
+++ turkle_site/settings.py
@@ -30,6 +30,9 @@ DATABASES = {
     }
 }
 
+# Use an Integer as the default primary key
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
