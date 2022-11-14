088f4698d5b935272ab93b58d426943bb919a5e6
Cash Costello <cash.costello@jhuapl.edu>
2022-11-14 11:14:52 -0500

    don't need to explicitly register token auth with changes to Turkle admin

--- turkle/api/admin.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.authtoken.admin import TokenAdmin
-from rest_framework.authtoken.models import TokenProxy
-
-from ..admin import admin_site
-
-admin_site.register(TokenProxy, TokenAdmin)
