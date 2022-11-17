00701988a28e8c817b8dc0f86991d6a241a840ee
Cash Costello <cash.costello@jhuapl.edu>
2022-11-17 12:11:53 -0500

    adds endpoint that publishes the API schema

--- turkle/api/urls.py
+++ turkle/api/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
+from rest_framework import permissions
+from rest_framework.schemas import get_schema_view
+from rest_framework.renderers import JSONOpenAPIRenderer
 
 from .views import BatchListCreate, BatchRetrieve, GroupListCreate, GroupRetrieve, ProjectListCreate, ProjectRetrieve,\
     UserListCreate, UserRetrieveUpdateDestroy
+from ..utils import get_site_name
+
+
+schema_view = get_schema_view(
+    title=get_site_name() + ' API',
+    version='1.0',
+    renderer_classes=[JSONOpenAPIRenderer],
+    permission_classes=(permissions.AllowAny,),
+    public=True
+)
 
 urlpatterns = [
+    path('schema.json', schema_view),
     path('batches/', BatchListCreate.as_view(), name='batches-list-create'),
     path('batches/<int:pk>', BatchRetrieve.as_view(), name='batches-details'),
     path('groups/', GroupListCreate.as_view(), name='groups-list-create'),
--- turkle_site/settings.py
+++ turkle_site/settings.py
@@ -125,6 +125,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
