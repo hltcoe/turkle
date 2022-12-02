223ea8d42a1d7839a0265f4f420b3a9f91cc49ae
Cash Costello <cash.costello@jhuapl.edu>
2022-12-02 11:54:42 -0500

    changed default format to json for API

--- turkle_site/settings.py
+++ turkle_site/settings.py
@@ -129,6 +129,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
     ]
 }
 
