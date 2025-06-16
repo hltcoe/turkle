720e489ed1b01f1f6bc27db8d168390bbd7db667
Cash Costello <cash.costello@gmail.com>
2025-06-16 06:21:38 -0400

    adds module name to sample logging config

--- turkle_site/example_local_settings.py
+++ turkle_site/example_local_settings.py
@@ -75,7 +75,7 @@
 #     },
 #     'formatters': {
 #         'simple': {
-#             'format': '%(asctime)s %(levelname)s: %(message)s',
+#             'format': '%(asctime)s %(levelname)s [%(name)s]: %(message)s',
 #             'datefmt': '%Y-%m-%d %H:%M:%S',
 #         },
 #     },
